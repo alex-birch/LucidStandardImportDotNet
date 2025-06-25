4351fb91385bad841c0c3f6c5cb46f67ff8b1388
andrew.casta <andrew.casta@xysense.com>
2025-06-25 13:27:35 +1000

    Add page index

--- src/model/LucidDocument.cs
+++ src/model/LucidDocument.cs
@@ -4,14 +4,14 @@ namespace LucidStandardImport.model
 {
     public class LucidDocument(string title)
     {
-        [JsonIgnore]
         // LucidIdFactory needs to be owned at the top level LucidDocument and passed to pages, to ensure uniqueness.
+        [JsonIgnore]
         public LucidIdFactory LucidIdFactory { get; } = new();
         public string Title { get; set; } = title;
         public int Version { get; set; } = 1;
         public IReadOnlyList<Page> Pages
         {
-            get { return _pages; }
+            get { return [.._pages.Where(p => p != null)]; }
         }
         public IReadOnlyList<Collection> Collections
         {
@@ -23,11 +23,21 @@ namespace LucidStandardImport.model
         private readonly List<Page> _pages = new List<Page>();
         private readonly List<Collection> _collections;
 
-        public Page AddPage(string title = null, PageSettings pageSettings = null)
+        public Page AddPage(string title = null, PageSettings pageSettings = null, int? index = null)
         {
             var page = new Page(LucidIdFactory, title, pageSettings);
             LucidIdFactory.AssignId(page);
-            _pages.Add(page);
+            if (!index.HasValue)
+                _pages.Add(page);
+            else
+            {
+                while (_pages.Count < index.Value)
+                    _pages.Add(null);
+                if (_pages.Count == index.Value)
+                    _pages.Add(page);
+                else
+                    _pages[index.Value] = page;
+            }
             return page;
         }
 
